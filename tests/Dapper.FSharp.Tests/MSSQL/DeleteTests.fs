67109790d6beb6e1ce46acda8edd8eb41faabe5c
Sampo Siltanen <sampo.siltanen@solita.fi>
2020-04-08 11:06:55 +0300

    Create more tests for output methods

--- tests/Dapper.FSharp.Tests/MSSQL/DeleteTests.fs
+++ tests/Dapper.FSharp.Tests/MSSQL/DeleteTests.fs
@@ -30,23 +30,6 @@ let tests (conn:IDbConnection) = Tests.testList "DELETE" [
         Expect.equal 9 (fromDb |> Seq.head |> fun (x:Persons.View) -> x.Position) ""
     }
 
-    testTask "Deletes and outputs single record" {
-        do! Persons.init conn
-        let rs = Persons.View.generate 10
-        let! _ =
-            insert {
-                table "Persons"
-                values rs
-            } |> conn.InsertAsync
-        let! fromDb =
-            delete {
-                table "Persons"
-                where (eq "Position" 10)
-            } |> conn.DeleteOutputAsync<Persons.View>
-        Expect.equal 1 (Seq.length fromDb) ""
-        Expect.equal 10 (fromDb |> Seq.head |> fun (x:Persons.View) -> x.Position) ""
-    }
-
     testTask "Deletes more records" {
         do! Persons.init conn
         let rs = Persons.View.generate 10
@@ -66,5 +49,46 @@ let tests (conn:IDbConnection) = Tests.testList "DELETE" [
                 table "Persons"
             } |> conn.SelectAsync<Persons.View>
         Expect.equal 6 (Seq.length fromDb) ""
+    }
+
+
+    /// OUTPUT tests
+
+    testTask "Deletes and outputs single record" {
+        do! Persons.init conn
+        let rs = Persons.View.generate 10
+        let! _ =
+            insert {
+                table "Persons"
+                values rs
+            } |> conn.InsertAsync
+        let! fromDb =
+            delete {
+                table "Persons"
+                where (eq "Position" 10)
+            } |> conn.DeleteOutputAsync<Persons.View>
+        Expect.equal 1 (Seq.length fromDb) ""
+        Expect.equal 10 (fromDb |> Seq.head |> fun (x:Persons.View) -> x.Position) ""
+    }
+
+    testTask "Deletes and outputs multiple records" {
+        do! Persons.init conn
+        let rs = Persons.View.generate 10
+        let! insertedPersonIds =
+            insert {
+                table "Persons"
+                values rs
+            } |> conn.InsertOutputAsync
+        let personIds = insertedPersonIds |> Seq.map (fun (p:{| Id:System.Guid |}) -> p.Id) |> Seq.toList
+        let boxedPersonIds = personIds |> List.map box |> Seq.toList
+
+        let! deleted =
+            delete {
+                table "Persons"
+                where (isIn "Id" boxedPersonIds)
+            } |> conn.DeleteOutputAsync<Persons.View>
+        Expect.hasLength deleted 10 ""
+        deleted |> Seq.iter (fun (p:Persons.View) ->
+            Expect.isTrue (personIds |> List.exists ((=) p.Id)) "Deleted personId not found from inserted Ids")
     }
 ]
