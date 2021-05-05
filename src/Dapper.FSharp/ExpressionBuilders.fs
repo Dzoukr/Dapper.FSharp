8ea9fca0ed96966ecd40107bfd3e46a31c75dd84
Jordan Marr <jmarr@microdesk.com>
2021-05-05 01:16:51 -0400

    Added a visitor active pattern

--- src/Dapper.FSharp/ExpressionBuilders.fs
+++ src/Dapper.FSharp/ExpressionBuilders.fs
@@ -1,36 +1,118 @@
 ﻿module Dapper.FSharp.ExpressionBuilders
 
 open System.Linq.Expressions
-                    
-let rec visit (exp: Expression) (where: Where) : Where =
+open System
+
+let (|Lambda|Unary|Binary|MethodCall|Constant|Member|Parameter|) (exp: Expression) =
     match exp.NodeType with
     | ExpressionType.Lambda -> 
-        let x = exp :?> LambdaExpression
-        visit x.Body where
-
+        Lambda (exp :?> LambdaExpression)
+    | ExpressionType.ArrayLength
+    | ExpressionType.Convert
+    | ExpressionType.ConvertChecked
+    | ExpressionType.Negate
+    | ExpressionType.UnaryPlus
+    | ExpressionType.NegateChecked
+    | ExpressionType.Not
+    | ExpressionType.Quote
     | ExpressionType.TypeAs ->
-        let x = exp :?> UnaryExpression
-        visit x.Operand where
-
-    //| ExpressionType.SubtractChecked -> 
-    //    let x = exp :?> BinaryExpression
-    //    let l = visit x.Left Where.Empty
-    //    let r = visit x.Right Where.Empty
-
-    //    let isRightSideNullConstant = 
-    //        x.Right.NodeType = (ExpressionType.Constant && (x.Right :?> ConstantExpression).Value = null)
-
-    //    match x.NodeType with
-    //    | ExpressionType.Equal when isRightSideNullConstant -> "IS"
-    //    | ExpressionType.NotEqual when isRightSideNullConstant -> "IS NOT"
-    //    | ExpressionType
-
-
+        Unary (exp :?> UnaryExpression)
+    | ExpressionType.Add
+    | ExpressionType.AddChecked
+    | ExpressionType.And
+    | ExpressionType.AndAlso
+    | ExpressionType.ArrayIndex
+    | ExpressionType.Coalesce
+    | ExpressionType.Divide
+    | ExpressionType.Equal
+    | ExpressionType.ExclusiveOr
+    | ExpressionType.GreaterThan
+    | ExpressionType.GreaterThanOrEqual
+    | ExpressionType.LeftShift
+    | ExpressionType.LessThan
+    | ExpressionType.LessThanOrEqual
+    | ExpressionType.Modulo
+    | ExpressionType.Multiply
+    | ExpressionType.MultiplyChecked
+    | ExpressionType.NotEqual
+    | ExpressionType.Or
+    | ExpressionType.OrElse
+    | ExpressionType.Power
+    | ExpressionType.RightShift
+    | ExpressionType.Subtract
+    | ExpressionType.SubtractChecked -> 
+        Binary (exp :?> BinaryExpression)
+    | ExpressionType.Call -> 
+        MethodCall (exp :?> MethodCallExpression)
+    | ExpressionType.Constant ->
+        Constant (exp :?> ConstantExpression)
+    | ExpressionType.MemberAccess ->
+        Member (exp :?> MemberExpression)
+    | ExpressionType.Parameter ->
+        Parameter (exp :?> ParameterExpression)
     | _ ->
-        failwith "Not Implemented"
-    
+        failwithf "Not implemented expression type: %A" exp.NodeType
 
-type SelectBuilder() =
+let rec visit (exp: Expression) : Where =
+    match exp with
+    | Lambda x ->
+        visit x.Body
+
+    | Unary x ->
+        visit x.Operand
+
+    | Binary x -> 
+        //let lt = visit x.Left
+
+        //let middle : BinaryOperation =
+        //    match x.Type with
+        //    | ExpressionType.Equal -> 
+
+        //let rt = visit x.Right
+
+        
+        //let isRightSideNullConstant = 
+        //    x.Right.NodeType = (ExpressionType.Constant && (x.Right :?> ConstantExpression).Value = null)
+
+        //match x.NodeType with
+        //| ExpressionType.Equal when isRightSideNullConstant -> "IS"
+        //| ExpressionType.NotEqual when isRightSideNullConstant -> "IS NOT"
+        //| ExpressionType
+        failwith "Not Implemented"
+
+    | MethodCall x -> 
+        failwith "Not Implemented"
+
+    | Constant x -> 
+        failwith "Not Implemented"
+
+    | Member x ->
+        failwith "Not Implemented"
+
+    | Parameter x -> 
+        failwith "Not Implemented"
+
+let visitWhere<'T> (filter: Expression<Func<'T, bool>>) =
+    visit (filter :> Expression)
+
+let tbl = Array.empty
+
+type SelectExpressionBuilder<'T>() =
+
+    let def = 
+        { Schema = None
+          Table = ""
+          Where = Where.Empty
+          OrderBy = []
+          Pagination = { Skip = 0; Take = None }
+          Joins = []
+          Aggregates = []
+          GroupBy = []
+          Distinct = false } : SelectQuery
+
+    member this.For (rows: seq<'T>, f: 'T -> SelectQuery) =
+        def
+
     member __.Yield _ =
         {
             Schema = None
@@ -45,73 +127,78 @@ type SelectBuilder() =
         } : SelectQuery
 
     /// Sets the TABLE name for query
-    [<CustomOperation "schema">]
+    [<CustomOperation("schema", MaintainsVariableSpace = true)>]
     member __.Schema (state:SelectQuery, name) = { state with Schema = Some name }
 
     /// Sets the TABLE name for query
-    [<CustomOperation "table">]
+    [<CustomOperation("table", MaintainsVariableSpace = true)>]
     member __.Table (state:SelectQuery, name) = { state with Table = name }
 
     /// Sets the WHERE condition
-    [<CustomOperation "where">]
-    member __.Where (state:SelectQuery, where:Where) = { state with Where = where }
+    [<CustomOperation("where", MaintainsVariableSpace = true)>]
+    member __.Where (state:SelectQuery, [<ProjectionParameter>] whereExpression) = 
+        let where = visitWhere<'T> whereExpression
+        { state with Where = where }
 
     /// Sets the ORDER BY for multiple columns
-    [<CustomOperation "orderByMany">]
+    [<CustomOperation("orderByMany", MaintainsVariableSpace = true)>]
     member __.OrderByMany (state:SelectQuery, values) = { state with OrderBy = values }
 
     /// Sets the ORDER BY for single column
-    [<CustomOperation "orderBy">]
+    [<CustomOperation("orderBy", MaintainsVariableSpace = true)>]
     member __.OrderBy (state:SelectQuery, colName, direction) = { state with OrderBy = [(colName, direction)] }
 
     /// Sets the SKIP value for query
-    [<CustomOperation "skip">]
+    [<CustomOperation("skip", MaintainsVariableSpace = true)>]
     member __.Skip (state:SelectQuery, skip) = { state with Pagination = { state.Pagination with Skip = skip } }
     
     /// Sets the TAKE value for query
-    [<CustomOperation "take">]
+    [<CustomOperation("take", MaintainsVariableSpace = true)>]
     member __.Take (state:SelectQuery, take) = { state with Pagination = { state.Pagination with Take = Some take } }
 
     /// Sets the SKIP and TAKE value for query
-    [<CustomOperation "skipTake">]
+    [<CustomOperation("skipTake", MaintainsVariableSpace = true)>]
     member __.SkipTake (state:SelectQuery, skip, take) = { state with Pagination = { state.Pagination with Skip = skip; Take = Some take } }
 
     /// INNER JOIN table where COLNAME equals to another COLUMN (including TABLE name)
-    [<CustomOperation "innerJoin">]
+    [<CustomOperation("innerJoin", MaintainsVariableSpace = true)>]
     member __.InnerJoin (state:SelectQuery, tableName, colName, equalsTo) = { state with Joins = state.Joins @ [InnerJoin(tableName, colName, equalsTo)] }
 
     /// LEFT JOIN table where COLNAME equals to another COLUMN (including TABLE name)
-    [<CustomOperation "leftJoin">]
+    [<CustomOperation("leftJoin", MaintainsVariableSpace = true)>]
     member __.LeftJoin (state:SelectQuery, tableName, colName, equalsTo) = { state with Joins = state.Joins @ [LeftJoin(tableName, colName, equalsTo)] }
     
     /// Sets the ORDER BY for multiple columns
-    [<CustomOperation "groupByMany">]
+    [<CustomOperation("groupByMany", MaintainsVariableSpace = true)>]
     member __.GroupByMany (state:SelectQuery, values) = { state with GroupBy = values }
 
     /// Sets the ORDER BY for single column
-    [<CustomOperation "groupBy">]
+    [<CustomOperation("groupBy", MaintainsVariableSpace = true)>]
     member __.GroupBy (state:SelectQuery, colName) = { state with GroupBy = [colName] }
 
     /// COUNT aggregate function for COLNAME (or * symbol) and map it to ALIAS
-    [<CustomOperation "count">]
+    [<CustomOperation("count", MaintainsVariableSpace = true)>]
     member __.Count (state:SelectQuery, colName, alias) = { state with Aggregates = state.Aggregates @ [Aggregate.Count(colName, alias)] }
 
     /// AVG aggregate function for COLNAME (or * symbol) and map it to ALIAS
-    [<CustomOperation "avg">]
+    [<CustomOperation("avg", MaintainsVariableSpace = true)>]
     member __.Avg (state:SelectQuery, colName, alias) = { state with Aggregates = state.Aggregates @ [Aggregate.Avg(colName, alias)] }
     
     /// SUM aggregate function for COLNAME (or * symbol) and map it to ALIAS
-    [<CustomOperation "sum">]
+    [<CustomOperation("sum", MaintainsVariableSpace = true)>]
     member __.Sum (state:SelectQuery, colName, alias) = { state with Aggregates = state.Aggregates @ [Aggregate.Sum(colName, alias)] }
     
     /// MIN aggregate function for COLNAME (or * symbol) and map it to ALIAS
-    [<CustomOperation "min">]
+    [<CustomOperation("min", MaintainsVariableSpace = true)>]
     member __.Min (state:SelectQuery, colName, alias) = { state with Aggregates = state.Aggregates @ [Aggregate.Min(colName, alias)] }
     
     /// MIN aggregate function for COLNAME (or * symbol) and map it to ALIAS
-    [<CustomOperation "max">]
+    [<CustomOperation("max", MaintainsVariableSpace = true)>]
     member __.Max (state:SelectQuery, colName, alias) = { state with Aggregates = state.Aggregates @ [Aggregate.Max(colName, alias)] }
     
     /// Sets query to return DISTINCT values
-    [<CustomOperation "distinct">]
+    [<CustomOperation("distinct", MaintainsVariableSpace = true)>]
     member __.Distinct (state:SelectQuery) = { state with Distinct = true }
+
+
+let select<'T> = SelectExpressionBuilder<'T>()
