28171fd308267e437a4e1d9e0dd0f5105ac7c472
Terence Hinrichsen <terence.hinrichsen@gmail.com>
2021-09-09 07:58:37 +0200

    Adds left join on many overload (#44)
    * Add source link
    
    * Bump version
    
    * Enable Source Link #18
    
    * Test using paket to add source link reference
    
    * Add tests back into build script
    
    * Add InnerJoin on two columns.
    
    * Add Innerjoin on many columns (List of tuples for ColName and eqToCol)
    
    * Up the version number to 17
    
    * Correct alignment issue and add innerJoinOnMany test.
    
    * Add test for InnerJoinOnMany. Tests successfully on MSSQL
    
    * Up SDK to .net5, update test project to .net5.
    
    * Add more tests after upgrading to .net5.0
    
    * Set SDK to V5 - latest minor.
    
    * Up minor version number
    
    * Add overload for Innerjoin with joinlist.
    
    * Add tests for inner join on many columns, could not test MySql
    
    * Add Left join keyword and builder overload on current Left join
    
    * Adds tests for LEFT JOIN on many
    
    * Add overload reference to Readme.
    
    Co-authored-by: Roland Andrag <roland.andrag@gmail.com>

--- src/Dapper.FSharp/PostgreSQL.fs
+++ src/Dapper.FSharp/PostgreSQL.fs
@@ -67,18 +67,19 @@ module private Evaluators =
         | { Take = None; Skip = o } -> sprintf "LIMIT ALL OFFSET %i" o
         | { Take = Some f; Skip = o } -> sprintf "LIMIT %i OFFSET %i" f o
 
-    let buildInnerJoinOnMany tableName (joinList: List<string * string>) =
+    let buildJoinOnMany joinType tableName (joinList: List<string * string>) =
         joinList
         |> List.map (fun (colName, eqToCol) -> sprintf "%s.%s=%s" (inQuotes tableName) (inQuotes colName) (inQuotes eqToCol))
         |> List.reduce (fun s1 s2 -> s1 + " AND " + s2 )
-        |> sprintf " INNER JOIN %s ON %s" (inQuotes tableName)
+        |> sprintf " %s JOIN %s ON %s" joinType (inQuotes tableName)
 
     let evalJoins (joins:Join list) =
         let sb = StringBuilder()
         let evalJoin = function
             | InnerJoin(table,colName,equalsTo) -> sprintf " INNER JOIN %s ON %s.%s=%s" (inQuotes table) (inQuotes table) (inQuotes colName) (inQuotes equalsTo)
             | LeftJoin(table,colName,equalsTo) -> sprintf " LEFT JOIN %s ON %s.%s=%s" (inQuotes table) (inQuotes table) (inQuotes colName) (inQuotes equalsTo)
-            | InnerJoinOnMany(table, list) -> buildInnerJoinOnMany table list
+            | InnerJoinOnMany(table, list) -> buildJoinOnMany "INNER" table list
+            | LeftJoinOnMany(table, list) -> buildJoinOnMany "LEFT" table list
         joins |> List.map evalJoin |> List.iter (sb.Append >> ignore)
         sb.ToString()
 
