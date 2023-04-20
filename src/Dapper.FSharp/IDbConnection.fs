2a5615a06d9298bfe1e1b745fb4592cfcce8f7bf
jindraivanek <jindra.ivanek@gmail.com>
2023-04-20 15:43:08 +0200

    CancellationToken support for all SelectAsync methods

--- src/Dapper.FSharp/IDbConnection.fs
+++ src/Dapper.FSharp/IDbConnection.fs
@@ -10,52 +10,52 @@ let query1<'a> (this:IDbConnection) trans timeout cancellationToken (logFunction
     CommandDefinition(query, pars, ?transaction = trans, ?commandTimeout = timeout, ?cancellationToken = cancellationToken)
     |> this.QueryAsync<'a>
 
-let query2<'a,'b> (this:IDbConnection) trans timeout (logFunction:LogFn option) (query, pars, splitOn) =
+let query2<'a,'b> (this:IDbConnection) trans timeout cancellationToken (logFunction:LogFn option) (query, pars, splitOn) =
     if logFunction.IsSome then (query, pars) |> logFunction.Value
-    this.QueryAsync<'a,'b,('a * 'b)>(query, (fun a b -> a,b), pars, splitOn = splitOn, ?transaction = trans, ?commandTimeout = timeout)
+    let cmd = CommandDefinition(query, pars, ?transaction = trans, ?commandTimeout = timeout, ?cancellationToken = cancellationToken)
+    this.QueryAsync<'a,'b,('a * 'b)>(cmd, (fun a b -> a,b), splitOn)
 
-let query3<'a,'b,'c> (this:IDbConnection) trans timeout (logFunction:LogFn option) (query, pars, splitOn) =
+let query3<'a,'b,'c> (this:IDbConnection) trans timeout cancellationToken (logFunction:LogFn option) (query, pars, splitOn) =
     if logFunction.IsSome then (query, pars) |> logFunction.Value
-    this.QueryAsync<'a,'b,'c,('a * 'b * 'c)>(query, (fun a b c -> a,b,c), pars, splitOn = splitOn, ?transaction = trans, ?commandTimeout =timeout)
+    let cmd = CommandDefinition(query, pars, ?transaction = trans, ?commandTimeout = timeout, ?cancellationToken = cancellationToken)
+    this.QueryAsync<'a,'b,'c,('a * 'b * 'c)>(cmd, (fun a b c -> a,b,c), splitOn)
     
-let query4<'a,'b,'c,'d> (this:IDbConnection) trans timeout (logFunction:LogFn option) (query, pars, splitOn) =
+let query4<'a,'b,'c,'d> (this:IDbConnection) trans timeout cancellationToken (logFunction:LogFn option) (query, pars, splitOn) =
     if logFunction.IsSome then (query, pars) |> logFunction.Value
-    this.QueryAsync<'a,'b,'c,'d,('a * 'b * 'c * 'd)>(query, (fun a b c d -> a,b,c,d), pars, splitOn = splitOn, ?transaction = trans, ?commandTimeout =timeout)
+    let cmd = CommandDefinition(query, pars, ?transaction = trans, ?commandTimeout = timeout, ?cancellationToken = cancellationToken)
+    this.QueryAsync<'a,'b,'c,'d,('a * 'b * 'c * 'd)>(cmd, (fun a b c d -> a,b,c,d), splitOn)
     
-let query5<'a,'b,'c,'d,'e> (this:IDbConnection) trans timeout (logFunction:LogFn option) (query, pars, splitOn) =
+let query5<'a,'b,'c,'d,'e> (this:IDbConnection) trans timeout cancellationToken (logFunction:LogFn option) (query, pars, splitOn) =
     if logFunction.IsSome then (query, pars) |> logFunction.Value
-    this.QueryAsync<'a,'b,'c,'d,'e,('a * 'b * 'c * 'd * 'e)>(query, (fun a b c d e -> a,b,c,d,e), pars, splitOn = splitOn, ?transaction = trans, ?commandTimeout =timeout)
+    let cmd = CommandDefinition(query, pars, ?transaction = trans, ?commandTimeout = timeout, ?cancellationToken = cancellationToken)
+    this.QueryAsync<'a,'b,'c,'d,'e,('a * 'b * 'c * 'd * 'e)>(cmd, (fun a b c d e -> a,b,c,d,e), splitOn)
 
-let query2Option<'a,'b> (this:IDbConnection) trans timeout (logFunction:LogFn option) (query, pars, splitOn) =
+let query2Option<'a,'b> (this:IDbConnection) trans timeout cancellationToken (logFunction:LogFn option) (query, pars, splitOn) =
     if logFunction.IsSome then (query, pars) |> logFunction.Value
-    this.QueryAsync<'a,'b,('a * 'b option)>(query, (fun x y -> x, Reflection.makeOption y), pars, splitOn = splitOn, ?transaction = trans, ?commandTimeout = timeout)
+    let cmd = CommandDefinition(query, pars, ?transaction = trans, ?commandTimeout = timeout, ?cancellationToken = cancellationToken)
+    this.QueryAsync<'a,'b,('a * 'b option)>(cmd, (fun x y -> x, Reflection.makeOption y), splitOn)
 
-let query3Option<'a,'b,'c> (this:IDbConnection) trans timeout (logFunction:LogFn option) (query, pars, splitOn) =
+let query3Option<'a,'b,'c> (this:IDbConnection) trans timeout cancellationToken (logFunction:LogFn option) (query, pars, splitOn) =
     if logFunction.IsSome then (query, pars) |> logFunction.Value
-    this.QueryAsync<'a,'b,'c,('a * 'b option * 'c option)>(query, (fun x y z -> x, Reflection.makeOption y, Reflection.makeOption z), pars, splitOn = splitOn, ?transaction = trans, ?commandTimeout = timeout)
+    let cmd = CommandDefinition(query, pars, ?transaction = trans, ?commandTimeout = timeout, ?cancellationToken = cancellationToken)
+    this.QueryAsync<'a,'b,'c,('a * 'b option * 'c option)>(cmd, (fun x y z -> x, Reflection.makeOption y, Reflection.makeOption z), splitOn)
 
-let query4Option<'a,'b,'c,'d> (this:IDbConnection) trans timeout (logFunction:LogFn option) (query, pars, splitOn) =
+let query4Option<'a,'b,'c,'d> (this:IDbConnection) trans timeout cancellationToken (logFunction:LogFn option) (query, pars, splitOn) =
     if logFunction.IsSome then (query, pars) |> logFunction.Value
+    let cmd = CommandDefinition(query, pars, ?transaction = trans, ?commandTimeout = timeout, ?cancellationToken = cancellationToken)
     this.QueryAsync<'a, 'b, 'c, 'd, ('a * 'b option * 'c option * 'd option)>(
-    query,
+    cmd,
     (fun a b c d -> a, Reflection.makeOption b, Reflection.makeOption c, Reflection.makeOption d),
-    pars,
-    splitOn = splitOn,
-    ?transaction = trans,
-    ?commandTimeout = timeout
-)
+    splitOn)
     
-let query5Option<'a,'b,'c,'d,'e> (this:IDbConnection) trans timeout (logFunction:LogFn option) (query, pars, splitOn) =
+let query5Option<'a,'b,'c,'d,'e> (this:IDbConnection) trans timeout cancellationToken (logFunction:LogFn option) (query, pars, splitOn) =
     if logFunction.IsSome then (query, pars) |> logFunction.Value
+    let cmd = CommandDefinition(query, pars, ?transaction = trans, ?commandTimeout = timeout, ?cancellationToken = cancellationToken)
     this.QueryAsync<'a, 'b, 'c, 'd, 'e, ('a * 'b option * 'c option * 'd option * 'e option)>(
-        query,
+        cmd,
         (fun a b c d e ->
             a, Reflection.makeOption b, Reflection.makeOption c, Reflection.makeOption d, Reflection.makeOption e),
-        pars,
-        splitOn = splitOn,
-        ?transaction = trans,
-        ?commandTimeout = timeout
-    )
+        splitOn)
 let execute (this:IDbConnection) trans timeout cancellationToken (logFunction:LogFn option) (query, values) =
     if logFunction.IsSome then (query, values) |> logFunction.Value
     CommandDefinition(query, values, ?transaction = trans, ?commandTimeout = timeout, ?cancellationToken = cancellationToken)
--- src/Dapper.FSharp/MSSQL/IDbConnection.fs
+++ src/Dapper.FSharp/MSSQL/IDbConnection.fs
@@ -11,29 +11,29 @@ type IDbConnection with
     member this.SelectAsync<'a> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction, ?cancellationToken : CancellationToken) =
         q |> Deconstructor.select<'a> |> IDbConnection.query1<'a> this trans timeout cancellationToken logFunction 
 
-    member this.SelectAsync<'a,'b> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction) =
-        q |> Deconstructor.select<'a,'b> |> IDbConnection.query2<'a,'b> this trans timeout logFunction
+    member this.SelectAsync<'a,'b> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction, ?cancellationToken : CancellationToken) =
+        q |> Deconstructor.select<'a,'b> |> IDbConnection.query2<'a,'b> this trans timeout cancellationToken logFunction
 
-    member this.SelectAsync<'a,'b,'c> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction) =
-        q |> Deconstructor.select<'a,'b,'c> |> IDbConnection.query3<'a,'b,'c> this trans timeout logFunction
+    member this.SelectAsync<'a,'b,'c> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction, ?cancellationToken : CancellationToken) =
+        q |> Deconstructor.select<'a,'b,'c> |> IDbConnection.query3<'a,'b,'c> this trans timeout cancellationToken logFunction
         
-    member this.SelectAsync<'a,'b,'c,'d> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction) =
-        q |> Deconstructor.select<'a,'b,'c,'d> |> IDbConnection.query4<'a,'b,'c,'d> this trans timeout logFunction
+    member this.SelectAsync<'a,'b,'c,'d> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction, ?cancellationToken : CancellationToken) =
+        q |> Deconstructor.select<'a,'b,'c,'d> |> IDbConnection.query4<'a,'b,'c,'d> this trans timeout cancellationToken logFunction
         
-    member this.SelectAsync<'a,'b,'c,'d,'e> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction) =
-        q |> Deconstructor.select<'a,'b,'c,'d,'e> |> IDbConnection.query5<'a,'b,'c,'d,'e> this trans timeout logFunction
+    member this.SelectAsync<'a,'b,'c,'d,'e> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction, ?cancellationToken : CancellationToken) =
+        q |> Deconstructor.select<'a,'b,'c,'d,'e> |> IDbConnection.query5<'a,'b,'c,'d,'e> this trans timeout cancellationToken logFunction
 
-    member this.SelectAsyncOption<'a,'b> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction) =
-        q |> Deconstructor.select<'a,'b>|> IDbConnection.query2Option<'a,'b> this trans timeout logFunction
+    member this.SelectAsyncOption<'a,'b> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction, ?cancellationToken : CancellationToken) =
+        q |> Deconstructor.select<'a,'b>|> IDbConnection.query2Option<'a,'b> this trans timeout cancellationToken logFunction
 
-    member this.SelectAsyncOption<'a,'b,'c> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction) =
-        q |> Deconstructor.select<'a,'b,'c> |> IDbConnection.query3Option<'a,'b,'c> this trans timeout logFunction
+    member this.SelectAsyncOption<'a,'b,'c> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction, ?cancellationToken : CancellationToken) =
+        q |> Deconstructor.select<'a,'b,'c> |> IDbConnection.query3Option<'a,'b,'c> this trans timeout cancellationToken logFunction
         
-    member this.SelectAsyncOption<'a,'b,'c,'d> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction) =
-        q |> Deconstructor.select<'a,'b,'c,'d> |> IDbConnection.query4Option<'a,'b,'c,'d> this trans timeout logFunction
+    member this.SelectAsyncOption<'a,'b,'c,'d> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction, ?cancellationToken : CancellationToken) =
+        q |> Deconstructor.select<'a,'b,'c,'d> |> IDbConnection.query4Option<'a,'b,'c,'d> this trans timeout cancellationToken logFunction
         
-    member this.SelectAsyncOption<'a,'b,'c,'d,'e> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction) =
-        q |> Deconstructor.select<'a,'b,'c,'d,'e> |> IDbConnection.query5Option<'a,'b,'c,'d,'e> this trans timeout logFunction
+    member this.SelectAsyncOption<'a,'b,'c,'d,'e> (q:SelectQuery, ?trans:IDbTransaction, ?timeout:int, ?logFunction, ?cancellationToken : CancellationToken) =
+        q |> Deconstructor.select<'a,'b,'c,'d,'e> |> IDbConnection.query5Option<'a,'b,'c,'d,'e> this trans timeout cancellationToken logFunction
 
     member this.InsertAsync<'a> (q:InsertQuery<'a>, ?trans:IDbTransaction, ?timeout:int, ?logFunction, ?cancellationToken : CancellationToken ) =
         q |> Deconstructor.insert<'a> |> IDbConnection.execute this trans timeout cancellationToken logFunction
